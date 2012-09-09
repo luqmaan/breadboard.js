ff51d96b7ff020c1ed3f073e946b1bc3e2e0b48a
createch <github@createchwebdesign.com>
2012-09-09 15:49:38 -0400

    Added a basic AND and OR gate

--- breadboard.js
+++ breadboard.js
@@ -1,10 +1,23 @@
+/* ====================== */
+/* PINS                   */
+/* ====================== */
+
+function pinVal ( pin ) {
+
+	return block[ pin.terminal ].value();
+}
+
+/* ====================== */
+/* STRIPS                 */
+/* ====================== */
+
 var Strip = Spine.Class.create({
 	init: function(args) {
-		this.row = args.row;
+		this.terminal = args.terminal;
 		this.block = args.block;
 	},
 	block: null,
-	row: null,
+	terminal: null,
 	pins: {
 		A: null,
 		B: null,
@@ -13,6 +26,7 @@ var Strip = Spine.Class.create({
 		E: 3
 	},
 	value: function() {
+
 		// returns the value of the current flowing through the strip
 		for (var pin in this.pins) {
 			if (this.pins[pin] !== null) {
@@ -34,11 +48,77 @@ var oddStrip = Strip.sub({
 	}
 });
 
+
+/* ====================== */
+/* GATES                  */
+/* ====================== */
+
+var Gates = {
+	Gate: Spine.Class.create({
+		init: function() {
+
+		},
+		input: [{
+			block: 0,
+			terminal: 15,
+			letter: 'F'
+		},{
+			block: 0,
+			terminal: 16,
+			letter: 'E'
+		}],
+		output: function() {
+			return null;
+		}
+	})
+};
+
+Gates.AND = Gates.Gate.sub({
+	output: function() {
+		return (pinVal(this.input[0]) === true) && (pinVal(this.input[1]) === true);
+	}
+});
+
+Gates.OR = Gates.Gate.sub({
+	output: function() {
+		return (pinVal(this.input[0]) === true) || (pinVal(this.input[1]) === true);
+	}
+});
+
+
+/* ====================== */
+/* Constructor            */
+/* ====================== */
+
 var strip1 = oddStrip.init({
-	block: 0,
-	row: 1
+	block: 1,
+	terminal: 1
 });
 var strip2 = evenStrip.init({
-	block: 0,
-	row: 2
+	block: 1,
+	terminal: 2
 });
+
+var block = [];
+
+for (var i = 0; i < 64; i++) {
+	if ( i%2 === 0 ) {
+		block.push(evenStrip.init({
+			block: 0,
+			terminal: i
+		}));
+	}
+	else {
+		block.push(oddStrip.init({
+			block: 0,
+			terminal: i
+		}));
+	}
+}
+
+
+block[15].pins.F = false;
+block[16].pins.E = true;
+
+var and = Gates.AND.init();
+var or = Gates.OR.init();
