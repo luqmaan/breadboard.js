3ff21eba99f363fc8f97a87c19350570e7bef036
Luqmaan <intern@dhcp20-171.laptops.usf.edu>
2012-12-10 20:35:07 -0500

    Starting to add condition to handle led

--- connected.js
+++ connected.js
@@ -28,6 +28,7 @@
 
 console.log ('hai')
 
+// set up the breadboard
 var breadboard = {
 	vcc: [{},{},{},{}],
 	vg: [{},{},{},{}],
@@ -35,6 +36,10 @@ var breadboard = {
 	b: [{},{},{},{}]
 }
 
+// i hate spelling queue
+var kew = [];
+
+// connect the strips
 breadboard.vcc[0].to = {
 	row: "a",
 	col: 0
@@ -58,30 +63,42 @@ breadboard.b[3].to = {
 	type: "resistor"
 }
 
+// loop through the rows starting with VCC and VG
 
 breadboard.vcc.forEach(function(pin, index, obj) {
 	"use strict";
-
 	handleTo(pin, 1, "vcc", index )
-
 })
 
 breadboard.vg.forEach(function(pin, index, obj) {
 	"use strict";
-
 	handleTo(pin, 0, "vg", index )
-
 })
 
-function out(row, col, pin) {
+breadboard.a.forEach(function(pin, index, obj) {
 	"use strict";
-	console.log (row + col)
-	console.log (pin)
-}
+	handleTo(pin, undefined, "a", index )
+})
 
+breadboard.b.forEach(function(pin, index, obj) {
+	"use strict";
+	handleTo(pin, undefined, "b", index )
+})
+
+console.log ( kew )
+
+// check if the pin is connected to something, and if it handle the connection appropriately
 function handleTo(pin, value, curRow, curCol) {
 	"use strict";
 
+	// if there is no value specified, we are on row A or B. pass the pins current value to the next pin
+	if (value === undefined)
+		value = breadboard[curRow][curCol].value
+
+	// if the pin is connected to something (and that something isn't special, like an LED, resistor or gate)
+	//    set the value of the thing its connected to
+	//    check if that pin is connected to something and repeat
+	//    update all the pins on that strip to have the value of the wire we've connected
 	if ( pin.to ) {
 
 		var r = pin.to.row
@@ -92,6 +109,10 @@ function handleTo(pin, value, curRow, curCol) {
 
 		console.log (curRow + curCol + " is connected to " + r + c + " which now has the value " + value)
 
+		if (pin.type === "led" || pin.type === "resistor") {
+			kew.push( {  } )
+		}
+
 		handleTo(nextPin, value, r, c)
 		handleStrip(r, c, value)
 
@@ -99,31 +120,34 @@ function handleTo(pin, value, curRow, curCol) {
 
 }
 
+
+// give all the pins on the current row the value 
 function handleStrip(row, col, value) {
 	"use strict";
 
-	var nextRow
-
 	// later make this a for loop that goes through all the other potential rows
 	// for now, since we have only rows A & B, we can just toggle a/b
-
-	// strip search
+	var nextRow
 	if (row === "a")
 		nextRow = "b"
 	else nextRow = "a"
 
+	// later add a condition to test if a pin on this row already has a value
+	// if it does, then throw an error. in real life you can't connect vcc into a strip twice
+
 	// update the value of the other pin on this strip
 	breadboard[nextRow][col].value = value
 
-	console.log ("all the pins on " + col + " have the value " + value + " because the pin on " + row + col + " had")
+	console.log ("all the pins on " + col + " have the value " + value + " because the pin on " + row + col)
 
 }
 
-
-
-
-
-
+// just for output
+function out(row, col, pin) {
+	"use strict";
+	console.log (row + col)
+	console.log (pin)
+}
 
 
 
